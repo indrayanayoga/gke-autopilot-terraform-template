9427a9e84de80e41fc0c83f7b30ef9e92aafc7e0
Indrayana Yogaputra <111721811+indrayanayoga@users.noreply.github.com>
2025-11-02 02:06:40 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "XXXXXXX"
+}
+
+variable "cluster_name" {
+  description = "GKE Cluster Name"
+  type        = string
+  default     = "YYYYYY"
+}
